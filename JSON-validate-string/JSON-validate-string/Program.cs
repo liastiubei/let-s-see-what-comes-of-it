4cdd777d2ccb1e7bc22653c0d897a6015ba15ab5
Lia Stiubei <liutzahp98@gmail.com>
2019-05-20 17:06:42 +0300

    Project revision

--- JSON-validate-string/JSON-validate-string/Program.cs
+++ JSON-validate-string/JSON-validate-string/Program.cs
@@ -13,50 +13,71 @@ namespace JSON_validate_string
         }
         public static bool CheckIfValidString(string jsonString)
         {
-            if (jsonString[0] != '\"' || jsonString[jsonString.Length-1] != '\"') return false;
-            for(int i=0;i<jsonString.Length;i++)
+            return HasQuotationMarksInBeginningAndEnd(jsonString)
+                && CharactersBiggerThan31(jsonString)
+                && CheckTheBackslash(jsonString)
+                && CorrectQuotationMarks(jsonString);
+        }
+
+        private static bool CorrectQuotationMarks(string jsonString)
+        {
+            for (int i = 1; i < jsonString.Length - 1; i++)
             {
-                if (jsonString[i] < 32) return false;
-            }
-            if (CheckTheBackslash(jsonString) == false) return false;
-            for(int i=1;i<jsonString.Length-1;i++)
-            {
-                if(jsonString[i]=='\"')
-                {
-                    if (jsonString[i - 1] != '\\') return false;
-                }
+                if (jsonString[i] == '\"' && jsonString[i - 1] != '\\')
+                    return false;
             }
             return true;
         }
+
+        private static bool CharactersBiggerThan31(string jsonString)
+        {
+            for (int i = 0; i < jsonString.Length; i++)
+            {
+                if (jsonString[i] < 32) return false;
+            }
+            return true;
+        }
+
+        private static bool HasQuotationMarksInBeginningAndEnd(string jsonString)
+        {
+            return jsonString[0] == '\"'
+                && jsonString[jsonString.Length - 1] == '\"';
+        }
+
         public static bool CheckTheBackslash(string jsonString)
         {
-            char[] backslash = { '\"', '\\', '\u002F', 'b', 'f', 'n', 'r', 't' };
+            string whatBackslashContainsAfter = "\"\\\u002Fbfnrt";
             for (int i = 0; i < jsonString.Length - 1; i++)
             {
                 bool p = false;int k = 0;
                 if (jsonString[i] == '\\')
                 {
-                    if (i > jsonString.Length - 3) return false;
-                    for (int j = 0; j < backslash.Length; j++)
+                    if (i > jsonString.Length - 3)
+                        return false;
+                    if(whatBackslashContainsAfter.Contains(jsonString[i+1]))
                     {
-                        if (jsonString[i + 1] == backslash[j])
-                        { p = true; k = 1; }
+                        p = true; k = 1;
                     }
                     if (jsonString[i + 1] == 'u')
                     {
-                        if (jsonString.Length - i - 2 < 4) return false;
+                        if (jsonString.Length - i - 2 < 4)
+                            return false;
                         int nr = 0;
                         for (int j = i + 2; j < i + 6; j++)
                         {
-                            if (jsonString[j] >= 48 && jsonString[j] <= 57) nr++;
-                            if (jsonString[j] >= 65 && jsonString[j] <= 90) nr++;
-                            if (jsonString[j] >= 97 && jsonString[j] <= 122) nr++;
+                            if ((jsonString[j] >= 48 && jsonString[j] <= 57)
+                                || (jsonString[j] >= 65 && jsonString[j] <= 90)
+                                || (jsonString[j] >= 97 && jsonString[j] <= 122))
+                                    nr++;
                         }
-                        if (nr == 4) { p = true; k = 4; }
+                        if (nr == 4)
+                        {
+                            p = true; k = 4;
+                        }
                     }
-                    if (p == false) return false;
+                    if (!p) return false;
                 }
-                i = i + k;
+                i += k;
             }
             return true;
         }
