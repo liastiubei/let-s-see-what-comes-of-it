76143660068909c6b509f6391bbbc7e54ff7fd2c
Lia Stiubei <liutzahp98@gmail.com>
2019-06-04 15:32:17 +0300

    Change 2 in Bubblesort solution

---
+++ BBS solution/BBS tests/Ranking.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BBS_tests
+{
+    class Ranking
+    {
+        private Team[] teams;
+
+        public Ranking(Team[] teams)
+        {
+            this.teams = teams;
+        }
+
+        public void Read(string[] lines)
+        {
+            for (int i = 0; i < this.teams.Length; i++)
+            {
+                this.teams[i].Read(lines[i]);
+            }
+        }
+
+        public bool Equal(Ranking that)
+        {
+            for (int i = 0; i < that.teams.Length; i++)
+            {
+                if (!this.teams[i].Equal(that.teams[i])) return false;
+            }
+            return true;
+        }
+
+    }
+}
---
+++ BBS solution/BBS tests/Ranking.tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace BBS_tests
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void CheckIfReadCorrectly_ShouldReturnTrue()
+        {
+            Team[] teams1 ={new Team("",0),
+                            new Team("",0),
+                            new Team("",0)};
+            Ranking rankings = new Ranking(teams1);
+            string[] lines = { "Echipa1 - 3 - 4",
+                            "Echipa2 - 13 - 5",
+                            "Echipa3 - 23 - 6"};
+            Team[] teams ={new Team("Echipa1 ", 7),
+                           new Team("Echipa2 ", 18),
+                           new Team("Echipa3 ", 29)};
+            Ranking correctRankings = new Ranking(teams);
+            rankings.Read(lines);
+            Assert.True(rankings.Equal(correctRankings));
+        }
+    }
+}
