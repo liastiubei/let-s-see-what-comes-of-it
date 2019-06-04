186665d64c3b36c8d73ed5573a65d3561621cce5
Lia Stiubei <liutzahp98@gmail.com>
2019-06-04 18:24:34 +0300

    First Game test

--- BBS solution/BBS tests/Game.tests.cs
+++ BBS solution/BBS tests/Game.tests.cs
@@ -6,9 +6,12 @@ namespace BBS_tests
     public class UnitTest3
     {
         [Fact]
-        public void ChecksIfItWorks()
+        public void ChecksIfNamesMatch1_ShouldReturnTrue()
         {
-            ;
+            int i = 0;
+            Game game = new Game("a", "b", 1, 2);
+            Assert.True(game.CheckIfNamesMatch1("a", ref i));
         }
+
     }
 }
