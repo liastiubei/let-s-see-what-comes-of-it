80ed869ed70e79b3db2e0590921c3895805ae258
Lia Stiubei <liutzahp98@gmail.com>
2019-06-20 14:56:07 +0300

    Choice test 3

--- Choice/Choice/UnitTest1.cs
+++ Choice/Choice/UnitTest1.cs
@@ -38,5 +38,23 @@ namespace Choice
                 );
             Assert.True(!digit.Match("a145"));
         }
+
+        [Fact]
+        public void CheckIfChoiceWorks3_ShouldReturnTrue()
+        {
+            var digit = new Choice(
+                new Character('0'),
+                new Range('1', '9')
+                );
+            var hex = new Choice(
+                digit,
+                new Choice(
+                    new Range('a', 'f'),
+                    new Range('A', 'F')
+                    ));
+            Assert.True(hex.Match("118932a"));
+        }
+
+
     }
 }
