ae03e2e37ef32f3f45b5c369148ed92be8727dc8
Lia Stiubei <liutzahp98@gmail.com>
2019-06-20 13:25:11 +0300

    character test2

--- Choice/Choice/UnitTest1.cs
+++ Choice/Choice/UnitTest1.cs
@@ -6,10 +6,17 @@ namespace Choice
     public class UnitTest1
     {
         [Fact]
-        public void CheckIfCharacterMatchWorks_ShouldReturnTrue()
+        public void CheckIfCharacterMatchWorks1_ShouldReturnTrue()
         {
             var character = new Character('a');
             Assert.True(character.Match("asparagus"));
+        }
+
+        [Fact]
+        public void CheckIfCharacterMatchWorks2_ShouldReturnFalse()
+        {
+            var character = new Character('a');
+            Assert.True(!character.Match("118932a"));
         }
     }
 }
