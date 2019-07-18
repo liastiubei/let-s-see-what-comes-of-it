276c6128900754b86eccfd03a105e0263c602ab7
Lia Stiubei <liutzahp98@gmail.com>
2019-07-19 00:37:15 +0300

    Class Any has been made (the tests as well)

---
+++ remake of range, choice, etc/remake of range, choice, etc/Any.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    public class AnyTests
+    {
+        [Fact]
+        public void CheckIfAnyWorksWith1Letter()
+        {
+            Any any = new Any("a");
+            Assert.True(any.Match("aura").Success());
+        }
+
+        [Fact]
+        public void CheckIfAnyWorksWithSeveralLetters()
+        {
+            Any any = new Any("alfabet");
+            Assert.True(any.Match("laura").Success());
+        }
+
+        [Fact]
+        public void CheckIfAnyWorksWithSeveralLettersShouldReturnFalse()
+        {
+            Any any = new Any("alfabet");
+            Assert.False(any.Match("nabucodonosor").Success());
+        }
+
+        [Fact]
+        public void CheckIfRemainingLettersAreCorrect()
+        {
+            Any any = new Any("alfabet");
+            Assert.Equal("aura", any.Match("laura").RemainingText());
+        }
+    }
+}
---
+++ remake of range, choice, etc/remake of range, choice, etc/Any.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class Any
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return new Match(false, text);
+            }
+
+            for (int i = 0; i < accepted.Length; i++)
+            {
+                if (text[0] == this.accepted[i])
+                {
+                    return new Match(true, text.Substring(1));
+                }
+            }
+
+            return new Match(false, text);
+        }
+    }
+}
