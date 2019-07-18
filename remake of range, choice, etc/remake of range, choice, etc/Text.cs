7a771515ec1f2050af327d3921cf6ad65e971bec
Lia Stiubei <liutzahp98@gmail.com>
2019-07-19 00:49:06 +0300

    Class test done

---
+++ remake of range, choice, etc/remake of range, choice, etc/Text.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class Text
+    {
+        private readonly string prefix;
+
+        public Text(string prefix)
+        {
+            this.prefix = prefix;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (string.IsNullOrEmpty(text) || text.Length < this.prefix.Length)
+            {
+                return new Match(false, text);
+            }
+
+            for (int i = 0; i < this.prefix.Length; i++)
+            {
+                if (text[i] != this.prefix[i])
+                {
+                   return new Match(false, text);
+                }
+            }
+
+            return new Match(true, text.Substring(this.prefix.Length));
+        }
+    }
+}
---
+++ remake of range, choice, etc/remake of range, choice, etc/Text.tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    public class TestTests
+    {
+        [Fact]
+        public void CheckIfTextWorksWith1Letter()
+        {
+            Text text = new Text("a");
+            Assert.True(text.Match("aura").Success());
+        }
+
+        [Fact]
+        public void CheckIfTextWorksWithSeveralLetters()
+        {
+            Text text = new Text("aur");
+            Assert.True(text.Match("aura").Success());
+        }
+
+        [Fact]
+        public void CheckIfTextWorksWithSeveralLettersShouldReturnFalse()
+        {
+            Text text = new Text("aur");
+            Assert.False(text.Match("nabuco").Success());
+        }
+
+        [Fact]
+        public void CheckIfRemainingLettersAreCorrect()
+        {
+            Text text = new Text("alfabet");
+            Assert.Equal("ar", text.Match("alfabetar").RemainingText());
+        }
+    }
+}
