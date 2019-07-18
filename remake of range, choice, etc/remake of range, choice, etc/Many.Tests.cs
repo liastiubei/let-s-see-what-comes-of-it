e293b51590b425fb9ec0abc147518bf5a70d917a
Lia Stiubei <liutzahp98@gmail.com>
2019-07-19 01:02:16 +0300

    Done with the Many Class&tests

---
+++ remake of range, choice, etc/remake of range, choice, etc/Many.Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    public class ManyTests
+    {
+        [Fact]
+        public void CheckIfManyReturnsTrueMatch()
+        {
+            Many many = new Many(new Character('a'));
+            Assert.True(many.Match("bcde").Success());
+        }
+
+        [Fact]
+        public void CheckIfManyConsumes1Letter()
+        {
+            Many many = new Many(new Character('a'));
+            Assert.Equal("bc", many.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void CheckIfManyConsumesSeveralLetters()
+        {
+            Many many = new Many(new Character('a'));
+            Assert.Equal("bc", many.Match("aaaaaaabc").RemainingText());
+        }
+    }
+}
---
+++ remake of range, choice, etc/remake of range, choice, etc/Many.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class Many
+    {
+        private readonly IPattern pattern;
+
+        public Many(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            while (this.pattern.Match(text).Success())
+            {
+                text = this.pattern.Match(text).RemainingText();
+            }
+
+            return new Match(true, text);
+        }
+    }
+}
