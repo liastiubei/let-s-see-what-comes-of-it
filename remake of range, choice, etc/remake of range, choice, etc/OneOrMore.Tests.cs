83db2a82d72db793d685c63716c2886dfc3e5beb
Lia Stiubei <liutzahp98@gmail.com>
2019-07-19 23:15:34 +0300

    OneOrMore class finished

---
+++ remake of range, choice, etc/remake of range, choice, etc/OneOrMore.Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    public class OneOrMoreTests
+    {
+        [Fact]
+        public void CheckIfItReturnsTrueIfItMatchesOne()
+        {
+            OneOrMore variable = new OneOrMore(new Character('a'));
+            Assert.True(variable.Match("abcde").Success());
+        }
+
+        [Fact]
+        public void CheckIfItReturnsTrueIfItMatchesSeveral()
+        {
+            OneOrMore variable = new OneOrMore(new Character('a'));
+            Assert.True(variable.Match("aaaaabcde").Success());
+        }
+
+        [Fact]
+        public void CheckIfItReturnsFalseIfItDoesntMatch()
+        {
+            OneOrMore variable = new OneOrMore(new Character('a'));
+            Assert.False(variable.Match("bcde").Success());
+        }
+
+        [Fact]
+        public void CheckIfRemainingTextIsCorrect()
+        {
+            OneOrMore variable = new OneOrMore(new Character('a'));
+            Assert.Equal("bcde", variable.Match("aaaabcde").RemainingText());
+        }
+    }
+}
---
+++ remake of range, choice, etc/remake of range, choice, etc/OneOrMore.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class OneOrMore
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            var oneOrMoreMatches = this.pattern.Match(text);
+            if (!oneOrMoreMatches.Success())
+            {
+                return new Match(false, text);
+            }
+
+            while (oneOrMoreMatches.Success())
+            {
+                oneOrMoreMatches = this.pattern.Match(oneOrMoreMatches.RemainingText());
+            }
+
+            return new Match(true, oneOrMoreMatches.RemainingText());
+        }
+    }
+}
