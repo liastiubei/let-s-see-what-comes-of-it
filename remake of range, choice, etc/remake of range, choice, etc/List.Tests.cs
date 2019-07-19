bbdc6ee00d25b4b6db9b5647bf0eac904a576d1a
Lia Stiubei <liutzahp98@gmail.com>
2019-07-19 23:42:25 +0300

    List class is done

---
+++ remake of range, choice, etc/remake of range, choice, etc/List.Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    public class ListTests
+    {
+        [Fact]
+        public void CheckIfItReturnsTrueIfElementMatches()
+        {
+            List variable = new List(new Character('a'), new Range('0', '9'));
+            Assert.True(variable.Match("abcde").Success());
+        }
+
+        [Fact]
+        public void CheckIfItReturnsTrueIfElementDoesntMatch()
+        {
+            List variable = new List(new Character('a'), new Range('0', '9'));
+            Assert.True(variable.Match("bcdefgh").Success());
+        }
+
+        [Fact]
+        public void CheckIfItRemovesTheCorrectLettersSoThatRemainingTextIsCorrect()
+        {
+            List variable = new List(new Character('a'), new Range('0', '9'));
+            Assert.Equal("bcde", variable.Match("a0a9a4a7abcde").RemainingText());
+        }
+    }
+}
---
+++ remake of range, choice, etc/remake of range, choice, etc/List.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class List
+    {
+        private readonly IPattern element;
+        private readonly IPattern separator;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.element = element;
+            this.separator = separator;
+        }
+
+        public IMatch Match(string text)
+        {
+            if (!this.element.Match(text).Success())
+            {
+                return new Match(true, text);
+            }
+
+            while (this.element.Match(text).Success())
+            {
+                text = this.element.Match(text).RemainingText();
+                if (this.separator.Match(text).Success())
+                {
+                    text = this.separator.Match(text).RemainingText();
+                }
+            }
+
+            return new Match(true, text);
+        }
+    }
+}
