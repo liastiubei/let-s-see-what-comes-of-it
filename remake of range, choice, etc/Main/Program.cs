913ce29a6802fb3753fbd993cf99edb4ec1df95c
Lia Stiubei <liutzahp98@gmail.com>
2019-08-12 14:15:05 +0300

    perfected main

--- remake of range, choice, etc/Main/Program.cs
+++ remake of range, choice, etc/Main/Program.cs
@@ -6,7 +6,7 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            string text = System.IO.File.ReadAllText(@Console.ReadLine());
+            string text = System.IO.File.ReadAllText(@args[0]);
             Json.Value newValue = new Json.Value();
             Console.WriteLine(newValue.Match(text).Success());
             Console.Read();
