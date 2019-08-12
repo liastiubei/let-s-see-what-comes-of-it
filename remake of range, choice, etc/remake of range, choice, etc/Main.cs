f82d483cc722544fadd596e513645e06eedc95aa
Lia Stiubei <liutzahp98@gmail.com>
2019-08-12 12:06:01 +0300

    Main problem

---
+++ remake of range, choice, etc/remake of range, choice, etc/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RemakeOfRangeChoiceEtc
+{
+    class ReadFromFile
+    {
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@Console.ReadLine());
+            var value = new Value();
+            Console.WriteLine(value.Match(text).Success());
+            Console.Read();
+        }
+    }
+}
