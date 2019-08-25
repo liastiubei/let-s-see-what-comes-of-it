85713e34eaf8f355133e08cc54efc97de96bcc8f
Lia Stiubei <liutzahp98@gmail.com>
2019-08-25 20:42:52 +0300

    objectArray + tests

---
+++ Data Structures/Data Structures/ObjectArray.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Structures
+{
+    public class ObjectArray
+    {
+        object[] array;
+
+        public ObjectArray()
+        {
+            this.array = new object[8];
+            Count = 0;
+        }
+
+        public object this[int index]
+        {
+            get => this.array[index];
+            set => this.array[index] = value;
+        }
+
+        public void Add(object element)
+        {
+            if (Count == array.Length)
+            {
+                Array.Resize(ref this.array, this.array.Length * 2);
+            }
+
+            this.array[Count] = element;
+            Count++;
+        }
+
+        public int Count { get; protected set; }
+
+        public object Element(int index)
+        {
+            return this.array[index];
+        }
+
+        public void SetElement(int index, int element)
+        {
+            this.array[index] = element;
+        }
+
+        public int IndexOf(object element)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (this.array[i] == element)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public bool Contains(int element)
+        {
+            if (this.IndexOf(element) != -1)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Insert(int index, object element)
+        {
+            if (Count > this.array.Length)
+            {
+                Array.Resize(ref this.array, this.array.Length * 2);
+            }
+
+            ShiftRight(index);
+            this.array[index] = element;
+            Count++;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(this.array, 0, this.array.Length);
+        }
+
+        public void Remove(object element)
+        {
+            RemoveAt(IndexOf(element));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index > Count - 1)
+            {
+                return;
+            }
+
+            ShiftLeft(index);
+            Array.Resize(ref this.array, this.array.Length - 1);
+            Count--;
+        }
+
+        public void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count; i++)
+            {
+                this.array[i] = this.array[i + 1];
+            }
+        }
+
+        public void ShiftRight(int index)
+        {
+            for (int i = Count; i > index; i--)
+            {
+                this.array[i] = this.array[i - 1];
+            }
+        }
+    }
+}
---
+++ Data Structures/DataStructuresTests/ObjectArray.Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+
+namespace DataStructuresTests
+{
+    public class ObjectArrayTests
+    {
+        Data_Structures.ObjectArray array = new Data_Structures.ObjectArray();
+
+        [Fact]
+        public void CheckIfAddWorksCorrectly1()
+        {
+            array.Add(1);
+            array.Add("hercules");
+            array.Add('a');
+            Assert.Equal(array.Element(0), 1);
+        }
+
+        [Fact]
+        public void CheckIfAddWorksCorrectly2()
+        {
+            array.Add(1);
+            array.Add("hercules");
+            array.Add('a');
+            Assert.Equal(array.Element(1), "hercules");
+        }
+
+        [Fact]
+        public void CheckIfAddWorksCorrectly3()
+        {
+            array.Add(1);
+            array.Add("hercules");
+            array.Add('a');
+            Assert.Equal(array.Element(2), 'a');
+        }
+    }
+}
