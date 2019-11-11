ce15e918cb83ce1be663627829fc569761173313
lia <liutzahp98@gmail.com>
2019-11-11 15:55:06 +0200

    DoubleLinkedList finished (all exceptions added + mistakes reviewed and fixed)

--- Data Structures/Data Structures/DoubleLinkedList.cs
+++ Data Structures/Data Structures/DoubleLinkedList.cs
@@ -19,6 +19,7 @@ namespace Data_Structures
             first = new DoubleLink<T>(default(T));
             first.NextLink = first;
             first.PreviousLink = first;
+            isReadOnly = false;
             Count = 0;
         }
         
@@ -46,74 +47,187 @@ namespace Data_Structures
             return null;
         }
 
-        private void AddBasic(DoubleLink<T> neededLink, DoubleLink<T> link)
+        public DoubleLink<T> FindLast(T findValue)
         {
-            link.NextLink = neededLink;
-            link.PreviousLink = neededLink.PreviousLink;
-            neededLink.PreviousLink.NextLink = link;
-            neededLink.PreviousLink = link;
-            Count++;
+            bool checkIfFound = false;
+            DoubleLink<T> searchLink = first.PreviousLink;
+            while (searchLink != first)
+            {
+                if (searchLink.value.Equals(findValue))
+                {
+                    checkIfFound = true;
+                    break;
+                }
+
+                searchLink = searchLink.PreviousLink;
+            }
+
+
+            if (checkIfFound)
+            {
+                return searchLink;
+            }
+
+            return null;
+        }
+
+        public void AddBefore(DoubleLink<T> neededLink, T item)
+        {
+            if (IsReadOnly)
+            {
+                throw new NotSupportedException("The array is readonly");
+            }
+
+            else if(neededLink == null)
+            {
+                throw new ArgumentNullException("The node is null");
+            }
+
+            else if(Find(neededLink.value) == null && neededLink != first)
+            {
+                throw new InvalidOperationException("The node is not in the current list");
+            }
+
+            else
+            {
+                DoubleLink<T> link = new DoubleLink<T>(item);
+                link.NextLink = neededLink;
+                link.PreviousLink = neededLink.PreviousLink;
+                neededLink.PreviousLink.NextLink = link;
+                neededLink.PreviousLink = link;
+                Count++;
+            }
         }
 
         public void Add(T item)
         {
-            DoubleLink<T> link = new DoubleLink<T>(item);
-            AddBasic(first, link);
-        }
-
-        public void AddBefore(DoubleLink<T> beforeLink, T item)
-        {
-            DoubleLink<T> link = new DoubleLink<T>(item);
-            AddBasic(beforeLink, link);
+            AddBefore(first, item);
         }
 
         public void AddAfter(DoubleLink<T> afterLink, T item)
         {
-            DoubleLink<T> link = new DoubleLink<T>(item);
-            AddBasic(afterLink.NextLink, link);
+            AddBefore(afterLink.NextLink, item);
         }
 
         public void AddFirst(T item)
         {
-            DoubleLink<T> link = new DoubleLink<T>(item);
-            AddBasic(first.NextLink, link);
+            AddBefore(first.NextLink, item);
         }
 
         public void Clear()
         {
-            first.value = default(T);
-            first.NextLink = first;
-            first.PreviousLink = first;
-            Count = 0;
+            if (isReadOnly)
+            {
+                throw new NotSupportedException("The list is readonly");
+            }
+
+            else
+            {
+                first.value = default(T);
+                first.NextLink = first;
+                first.PreviousLink = first;
+                Count = 0;
+            }
         }
 
         public bool Contains(T item)
         {
-            return this.Find(item) != null
-                ? true
-                : false;
+            return this.Find(item) != null;
         }
 
         public void CopyTo(T[] array, int arrayIndex)
         {
-            DoubleLink<T> link = first.NextLink;
-            for(int i = arrayIndex; i < arrayIndex + Count; i++)
+            if(array == null)
             {
-                array[i] = link.value;
-                link = link.NextLink;
+                throw new ArgumentNullException("Array is null");
+            }
+            
+            else if(arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index is less than zero");
+            }
+
+            else if(Count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("The number of elements in the list is greater than the available space from index to the end of the array");
+            }
+
+            else
+            {
+                DoubleLink<T> link = first.NextLink;
+                for (int i = arrayIndex; i < arrayIndex + Count; i++)
+                {
+                    array[i] = link.value;
+                    link = link.NextLink;
+                }
             }
         }
 
         public bool Remove(T item)
         {
-            DoubleLink<T> link = this.Find(item);
-            link.PreviousLink.NextLink = link.NextLink;
-            link.NextLink.PreviousLink = link.PreviousLink;
-            link = null;
-            Count--;
+            if (IsReadOnly)
+            {
+                throw new NotSupportedException("The array is readonly");
+            }
+
+            else
+            {
+                DoubleLink<T> link = this.Find(item);
+                link.PreviousLink.NextLink = link.NextLink;
+                link.NextLink.PreviousLink = link.PreviousLink;
+                link = null;
+                Count--;
+            }
             return true;
         }
 
+        public bool RemoveFirst()
+        {
+            if (IsReadOnly)
+            {
+                throw new NotSupportedException("The array is readonly");
+            }
+
+            else if (first.NextLink == first)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            else
+            {
+                first.NextLink = first.NextLink.NextLink;
+                first.NextLink.PreviousLink = first;
+                Count--;
+                return true;
+            }
+        }
+
+        public bool RemoveLast()
+        {
+            if (IsReadOnly)
+            {
+                throw new NotSupportedException("The array is readonly");
+            }
+
+            else if (first.NextLink == first)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            else
+            {
+                first.PreviousLink = first.PreviousLink.PreviousLink;
+                first.PreviousLink.NextLink = first;
+                Count--;
+                return true;
+            }
+        }
+
+        public bool Equals(object obj)
+        {
+            return obj == this;
+        }
+        
         public IEnumerator<T> GetEnumerator()
         {
             DoubleLink<T> link = first.NextLink;
--- Data Structures/DataStructuresTests/DoubleLinkedListTests.cs
+++ Data Structures/DataStructuresTests/DoubleLinkedListTests.cs
@@ -100,5 +100,185 @@ namespace DataStructuresTests
             linkedList.Remove(2);
             Assert.Equal(3, linkedList.Find(1).NextLink.value);
         }
+
+        [Fact]
+        public void CheckIfRemoveFirstWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.RemoveFirst();
+            Assert.Equal(null, linkedList.Find(1));
+        }
+
+        [Fact]
+        public void CheckIfRemoveLastWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.RemoveLast();
+            Assert.Equal(null, linkedList.Find(3));
+        }
+
+        [Fact]
+        public void CheckIfEqualsWorksCorrectly1()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var equalObject = linkedList;
+            Assert.True(linkedList.Equals(equalObject));
+        }
+
+        [Fact]
+        public void CheckIfEqualsWorksCorrectly2()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var equalObject = new Data_Structures.DoubleLinkedList<int>(); ;
+            equalObject.Add(4);
+            Assert.False(equalObject.Equals(linkedList));
+        }
+
+        [Fact]
+        public void CheckIfFindLastWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(2);
+            Assert.Equal(linkedList.Find(2).NextLink.NextLink, linkedList.FindLast(2));
+        }
+
+        [Fact]
+        public void CheckIfAddBeforeExceptionReadOnlyWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.MakeReadOnly(); 
+            Assert.Throws<NotSupportedException>(() => linkedList.AddBefore(linkedList.Find(3), 2));
+        }
+
+        [Fact]
+        public void CheckIfAddBeforeArgumentNullExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            Assert.Throws<ArgumentNullException>(() => linkedList.AddBefore(null, 2));
+        }
+
+        [Fact]
+        public void CheckIfAddBeforeInvalidOperationExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var newLink = new Data_Structures.DoubleLink<int>(5);
+            Assert.Throws<InvalidOperationException>(() => linkedList.AddBefore(newLink, 2));
+        }
+
+        [Fact]
+        public void CheckIfClearReadonlyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.MakeReadOnly();
+            Assert.Throws<NotSupportedException>(() => linkedList.Clear());
+        }
+
+        [Fact]
+        public void CheckIfCopyToNullArrayExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var array = new int[2];
+            array = null;
+            Assert.Throws<ArgumentNullException>(() => linkedList.CopyTo(array, 1));
+        }
+
+        [Fact]
+        public void CheckIfCopyToIndexSmallerThanZeroExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var array = new int[20];
+            Assert.Throws<ArgumentOutOfRangeException>(() => linkedList.CopyTo(array, -10));
+        }
+
+        [Fact]
+        public void CheckIfCopyToNotEnoughSpaceExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            var array = new int[3];
+            Assert.Throws<ArgumentException>(() => linkedList.CopyTo(array, 1));
+        }
+
+        [Fact]
+        public void CheckIfRemoveReadonlyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.MakeReadOnly();
+            Assert.Throws<NotSupportedException>(() => linkedList.Remove(2));
+        }
+
+        [Fact]
+        public void CheckIfRemoveFirstReadonlyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.MakeReadOnly();
+            Assert.Throws<NotSupportedException>(() => linkedList.RemoveFirst());
+        }
+
+        [Fact]
+        public void CheckIfRemoveFirstEmptyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            Assert.Throws<InvalidOperationException>(() => linkedList.RemoveFirst());
+        }
+
+        [Fact]
+        public void CheckIfRemoveLastReadonlyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.MakeReadOnly();
+            Assert.Throws<NotSupportedException>(() => linkedList.RemoveLast());
+        }
+
+        [Fact]
+        public void CheckIfRemoveLastEmptyExceptionWorksCorrectly()
+        {
+            var linkedList = new Data_Structures.DoubleLinkedList<int>();
+            Assert.Throws<InvalidOperationException>(() => linkedList.RemoveLast());
+        }
     }
 }
