538a76e3bf48aa261fd61e369c9ede2b5269b56d
Lia Stiubei <liutzahp98@gmail.com>
2020-01-26 13:49:32 +0200

    SelectMany finished

--- Data Structures/Data Structures/LinqFunctions.cs
+++ Data Structures/Data Structures/LinqFunctions.cs
@@ -9,6 +9,11 @@ namespace DataStructures
     {
         public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException("Source or Predicate null");
+            }
+
             foreach (var obj in source)
             {
                 if (!predicate(obj))
@@ -22,6 +27,11 @@ namespace DataStructures
 
         public static bool Any<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source null");
+            }
+
             foreach (var obj in source)
             {
                 if (predicate(obj))
@@ -35,15 +45,82 @@ namespace DataStructures
 
         public static TSource First<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
         {
-            if (Count<TSource>(source) == 0)
+            if (source == null || predicate == null)
+            {
+                throw new ArgumentNullException("Source or Predicate is null");
+            }
+
+            if (SimpleCount<TSource>(source) == 0)
             {
                 throw new InvalidOperationException("The enumeration has no elements");
             }
 
-            return predicate(source.First()) ? source.First() : default;
+            TSource variable = default;
+
+            foreach (var obj in source)
+            {
+                if (predicate(obj))
+                {
+                    variable = obj;
+                    break;
+                }
+            }
+
+            return variable;
         }
 
-        public static int Count<TSource>(this IEnumerable<TSource> source)
+        public static IEnumerable<TResult> Select<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+        {
+            if (source == null || selector == null)
+            {
+                throw new ArgumentNullException("The Source or the Selector is null");
+            }
+
+            TResult[] output = new TResult[SimpleCount<TSource>(source)];
+            int i = 0;
+            foreach (var obj in source)
+            {
+                output[i++] = selector(obj);
+            }
+
+            return output;
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            if (source == null || selector == null)
+            {
+                throw new ArgumentNullException("The Source or the Selector is null");
+            }
+
+            TResult[] output = new TResult[CountWithSelector<TSource, TResult>(source, selector)];
+            int i = 0;
+            foreach (var obj in source)
+            {
+                foreach (var selectorObject in selector(obj))
+                {
+                    output[i++] = selectorObject;
+                }
+            }
+
+            return output;
+        }
+
+        public static int CountWithSelector<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            int num = 0;
+            foreach (var obj in source)
+            {
+                foreach (var selectedObj in selector(obj))
+                {
+                    num++;
+                }
+            }
+
+            return num;
+        }
+
+        public static int SimpleCount<TSource>(this IEnumerable<TSource> source)
         {
             int num = 0;
             foreach (var obj in source)
--- Data Structures/DataStructuresTests/LinqFunctionsTests.cs
+++ Data Structures/DataStructuresTests/LinqFunctionsTests.cs
@@ -32,6 +32,14 @@ namespace DataStructuresTests
         }
 
         [Fact]
+        public void CheckIfAllArgumentsWorkCorrectly()
+        {
+            DataStructures.ListCollection<string> array = null;
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.Throws<ArgumentNullException>(() => DataStructures.LinqFunctions.All<string>(array, isUpperCase));
+        }
+
+        [Fact]
         public void CheckIfAnyWorksCorrectly1()
         {
             DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
@@ -56,6 +64,14 @@ namespace DataStructuresTests
         }
 
         [Fact]
+        public void CheckIfAnyArgumentsWorksCorrectly()
+        {
+            DataStructures.ListCollection<string> array = null;
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.Throws<ArgumentNullException>(() => DataStructures.LinqFunctions.Any<string>(array, isUpperCase));
+        }
+
+        [Fact]
         public void CheckIfFirstWorksCorrectly1()
         {
             DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
@@ -72,11 +88,171 @@ namespace DataStructuresTests
         {
             DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
             array.Add("ana");
-            array.Add("paul");
+            array.Add("Paul");
             array.Add("zero");
             array.Add("penelope");
             Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
-            Assert.True(DataStructures.LinqFunctions.First<string>(array, isUpperCase) == null);
+            Assert.True(DataStructures.LinqFunctions.First<string>(array, isUpperCase) == "Paul");
+        }
+
+        [Fact]
+        public void CheckIfFirstArgumentsWorksCorrectly1()
+        {
+            DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.Throws<InvalidOperationException>(() => DataStructures.LinqFunctions.First<string>(array, isUpperCase) == "Ana");
+        }
+
+        [Fact]
+        public void CheckIfFirstArgumentsWorksCorrectly2()
+        {
+            DataStructures.ListCollection<string> array = null;
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.Throws<ArgumentNullException>(() => DataStructures.LinqFunctions.First<string>(array, isUpperCase));
+        }
+
+        [Fact]
+        public void CheckIfSelectArgumentsWorksCorrectly()
+        {
+            DataStructures.ListCollection<string> array = null;
+            Func<string, string> makeUpperCase = x =>
+            {
+                string y = x;
+                if (x[0] >= 97 && x[0] <= 122)
+                {
+                    y = x[0].ToString().ToUpper() + x.Substring(1, x.Length - 1);
+                }
+
+                return y;
+            };
+
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.Throws<ArgumentNullException>(() => DataStructures.LinqFunctions.All(DataStructures.LinqFunctions.Select(array, makeUpperCase), isUpperCase));
+        }
+
+        [Fact]
+        public void CheckIfSelectWorksCorrectly1()
+        {
+            DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
+            array.Add("ana");
+            array.Add("paul");
+            array.Add("zero");
+            array.Add("penelope");
+            Func<string, string> makeUpperCase = x =>
+            {
+                string y = x;
+                if (x[0] >= 97 && x[0] <= 122)
+                {
+                    y = x[0].ToString().ToUpper() + x.Substring(1, x.Length - 1);
+                }
+
+                return y;
+            };
+
+            Func<string, bool> isUpperCase = x => x[0] >= 65 && x[0] <= 90;
+            Assert.True(DataStructures.LinqFunctions.All(DataStructures.LinqFunctions.Select(array, makeUpperCase), isUpperCase));
+        }
+
+        [Fact]
+        public void CheckIfSelectWorksCorrectly2()
+        {
+            DataStructures.ListCollection<string> array = new DataStructures.ListCollection<string>();
+            array.Add("ana");
+            array.Add("paul");
+            array.Add("zero");
+            array.Add("penelope");
+            Func<string, string> makeUpperCase = x =>
+            {
+                string y = x;
+                if (x[0] >= 97 && x[0] <= 122)
+                {
+                    y = x[0].ToString().ToUpper() + x.Substring(1, x.Length - 1);
+                }
+
+                return y;
+            };
+
+            Func<string, bool> isLowerCase = x => x[0] >= 97 && x[0] <= 122;
+            Assert.False(DataStructures.LinqFunctions.Any(DataStructures.LinqFunctions.Select(array, makeUpperCase), isLowerCase));
+        }
+
+        [Fact]
+        public void CheckIfSelectManyArgumentsWorksCorrectly()
+        {
+            DataStructures.ListCollection<List<string>> array = null;
+            Func<List<string>, List<List<string>>> makeUpperCaseFirstAndAll = x =>
+            {
+                List<List<string>> returnList = new List<List<string>>();
+                foreach (var obj in x)
+                {
+                    string first = obj;
+                    if (obj[0] >= 97 && obj[0] <= 122)
+                    {
+                        first = obj[0].ToString().ToUpper() + obj.Substring(1, obj.Length - 1);
+                    }
+
+                    string all = obj.ToUpper();
+                    returnList.Add(new List<string> { first, all });
+                }
+
+                return returnList;
+            };
+
+            Func<List<string>, bool> isFirstUpperCase = x =>
+            {
+                foreach (var obj in x)
+                {
+                    if (!(obj[0] >= 65 && obj[0] <= 90))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+
+            Assert.Throws<ArgumentNullException>(() => DataStructures.LinqFunctions.SelectMany<List<string>, List<string>>(array, makeUpperCaseFirstAndAll));
+        }
+
+        [Fact]
+        public void CheckIfSelectManyWorksCorrectly()
+        {
+            DataStructures.ListCollection<List<string>> array = new DataStructures.ListCollection<List<string>>();
+            array.Add(new List<string> { "Ana", "alex", "Andrei" });
+            array.Add(new List<string> { "Paul", "paula", "Pelena", "penelope" });
+            array.Add(new List<string> { "zero", "Zen", "zohan" });
+            Func<List<string>, List<List<string>>> makeUpperCaseFirstAndAll = x =>
+            {
+                List<List<string>> returnList = new List<List<string>>();
+                foreach (var obj in x)
+                {
+                    string first = obj;
+                    if (obj[0] >= 97 && obj[0] <= 122)
+                    {
+                        first = obj[0].ToString().ToUpper() + obj.Substring(1, obj.Length - 1);
+                    }
+
+                    string all = obj.ToUpper();
+                    returnList.Add(new List<string> { first, all });
+                }
+
+                return returnList;
+            };
+
+            Func<List<string>, bool> isFirstUpperCase = x =>
+            {
+                foreach (var obj in x)
+                {
+                    if (!(obj[0] >= 65 && obj[0] <= 90))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            };
+
+            Assert.True(DataStructures.LinqFunctions.All(DataStructures.LinqFunctions.SelectMany<List<string>, List<string>>(array, makeUpperCaseFirstAndAll), isFirstUpperCase));
         }
     }
 }
