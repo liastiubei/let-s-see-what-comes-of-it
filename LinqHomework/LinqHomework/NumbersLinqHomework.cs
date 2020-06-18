f985e3cf50003abfc5b6c3019d7c345c68faaa72
Lia Stiubei <liutzahp98@gmail.com>
2020-06-18 21:09:33 +0300

    ex 7 first draft*

--- LinqHomework/LinqHomework/NumbersLinqHomework.cs
+++ LinqHomework/LinqHomework/NumbersLinqHomework.cs
@@ -8,13 +8,12 @@ namespace LinqHomework
     {
         public static IEnumerable<IEnumerable<int>> AllSubsequencesWithSumEqualOrSmallerThanK(this IEnumerable<int> array, int k)
         {
-            var x = from i in Enumerable.Range(0, array.Count())
+            return from i in Enumerable.Range(0, array.Count())
                    from j in Enumerable.Range(i, array.Count() - i)
                    let elements = array.Skip(i).Take(j - i + 1)
                    let sum = elements.Sum()
                    where sum <= k
                    select elements;
-            return x.ToList();
         }
     }
 }
