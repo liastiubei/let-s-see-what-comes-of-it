6db4543b0f1c43131596f6bf63d048e038355533
Lia Stiubei <liutzahp98@gmail.com>
2020-06-18 21:08:31 +0300

    Ex 7 first draft

---
+++ LinqHomework/LinqHomework/NumbersLinqHomework.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqHomework
+{
+    public static class NumbersLinqHomework
+    {
+        public static IEnumerable<IEnumerable<int>> AllSubsequencesWithSumEqualOrSmallerThanK(this IEnumerable<int> array, int k)
+        {
+            var x = from i in Enumerable.Range(0, array.Count())
+                   from j in Enumerable.Range(i, array.Count() - i)
+                   let elements = array.Skip(i).Take(j - i + 1)
+                   let sum = elements.Sum()
+                   where sum <= k
+                   select elements;
+            return x.ToList();
+        }
+    }
+}
---
+++ LinqHomework/LinqHomeworkTests/NumbersLinqHomeworkTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace LinqHomeworkTests
+{
+    public class NumbersLinqHomeworkTests
+    {
+        [Fact]
+        public void CheckIfAllSubsequencesWithSumEqualOrSmallerThanKWorksCorrectly()
+        {
+            int[] array = { 1, 5, 3, 8};
+            var result = new List<IEnumerable<int>>();
+            result.Add(new int[] { 1 });
+            result.Add(new int[] { 1, 5 });
+            result.Add(new int[] { 5 });
+            result.Add(new int[] { 3 });
+            var x = LinqHomework.NumbersLinqHomework.AllSubsequencesWithSumEqualOrSmallerThanK(array, 7);
+            Assert.Equal(result, x);
+        }
+    }
+}
